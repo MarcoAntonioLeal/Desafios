cc4bf697a36c93498047409eb442780f10a6b2e9
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-08-01 22:40:48 -0300

    atualizando desafio

--- desafio01/inventarioMagico/index.js
+++ desafio01/inventarioMagico/index.js
@@ -4,27 +4,39 @@ const btnDel = document.querySelector('#btn-del')
 const btnList = document.querySelector('#btn-list')
 const listaDeItens = document.querySelector('#listaItens')
 
+
 const inventario = []
-
-
 
 btnAdd.addEventListener('click', () => {
     const li = document.createElement('li')
-    li.setAttribute('id', fieldText.value)//tem de ser o index e não o nome
+    li.setAttribute('id', inventario.length)
+    li.setAttribute('class', 'listas')
     listaDeItens.appendChild(li).textContent = fieldText.value
     
     inventario.push(fieldText.value)
 
     console.log(li) // teste
     console.log(inventario) // teste
+
     fieldText.value = ''
 })
 
 btnDel.addEventListener('click', () => {
-    console.log('Mozinho Lindo') /*lembrar que o id da lista será o indexof() -1 */
+    //delete item array
+    let indice = inventario.indexOf(fieldText.value)
+    inventario.splice(indice, 1)
+
+    //delete lista
+    let allLists = document.querySelectorAll('.listas')
+    allLists.forEach(li => console.log(li.id -1))
+
+    console.log(indice) //teste
+    console.log(inventario)//teste
+    console.log(allLists)//teste
+
+    fieldText.value = ''
 })
 
 btnList.addEventListener('click', () => {
     console.log('Quero chocolate')
 })
-
