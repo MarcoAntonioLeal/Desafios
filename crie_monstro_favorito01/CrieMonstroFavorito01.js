7b2dca2901021a4314d954d23f39d020557548bf
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-08-02 15:49:31 -0300

    atualizando desafio

--- crie_monstro_favorito01/CrieMonstroFavorito01.js
+++ crie_monstro_favorito01/CrieMonstroFavorito01.js
@@ -5,10 +5,6 @@ class monstro {
         this.nivel = nivel
         this.habilidade = habilidade
     }
-    novoMonstro() {}
 }
 
-
-/**O monstro  é um  de nível  e possui a habilidade especial: .
-Exemplo:
-O monstro Dragão é um Dragão de nível 85 e possui a habilidade especial: Vôo de Fogo. */
+console.log(new monstro('charmander', 'fogo', 30, 'Bola de fogo'))
