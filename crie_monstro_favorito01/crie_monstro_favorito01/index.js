48e4125f8913852abf044ff27471dcc1ecf21a32
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-08-09 00:56:36 -0300

    desafio concluído

--- crie_monstro_favorito01/crie_monstro_favorito01/index.js
+++ crie_monstro_favorito01/crie_monstro_favorito01/index.js
@@ -10,9 +10,30 @@ const resultado = document.querySelector('#resultado')
 const btnCriar = document.querySelector('#btn-criar')
 
 btnCriar.addEventListener('click', () => {
-    class monstro {
-        constructor() {
 
+    if(nome.value == '' || nivel.value == '' || especial.value == '' || tipo == '') {
+        alert('Todos os campos tem de ser preenchidos')
+    } else if (nivel.value < 1 || nivel.value > 100) {
+        alert('Valores do nível tem de ser maior que 1 e menor que 100')
+    } else {
+        class monstro {
+            constructor() {
+                this.nome = nome.value
+                this.nivel = nivel.value
+                this.especial = especial.value
+                this.tipo = tipo
+            }
         }
+    
+        let novoMonstro = new monstro(nome, tipo, nivel, especial)
+            
+        resultado.innerHTML += `
+        O monstro ${novoMonstro.nome} é um ${novoMonstro.tipo} de nível ${novoMonstro.nivel} e possui a habilidade especial: ${novoMonstro.especial} 
+        <br>`
     }
+
+    nome.value = ''
+    nivel.value = ''
+    especial.value = ''
+    document.querySelector('#tipo').value = ''
 })
