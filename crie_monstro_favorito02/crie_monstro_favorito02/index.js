4341af6eb34c5d379e93b2bb7391fb5e7549ed30
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-08-15 22:30:24 -0300

    atualizando desafio

--- crie_monstro_favorito02/crie_monstro_favorito02/index.js
+++ crie_monstro_favorito02/crie_monstro_favorito02/index.js
@@ -1,19 +1,31 @@
 const nome = document.querySelector('#nome')
 const nivel = document.querySelector('#nivel')
-
-let tipo = ''
-document.querySelector('#tipo').addEventListener('change', (ev) => {
-    tipo = ev.currentTarget.value
-})
+const resultado = document.querySelector('#resultado')
 
 let elemento = ''
+document.querySelector('#elemento').addEventListener('change', (ev) => {
+    elemento = ev.currentTarget.value
+})
 
-const resultado = document.querySelector('#resultado')
+/*let habilidade = ''
+document.querySelector('#habilidade').addEventListener('', (ev) => {
+    habilidade = ev.currentTarget.value
+    resultado.textContent = 'oi'
+})*/
+
+document.querySelector('#habilidade').addEventListener('input', (ev) => {
+    console.log(ev.currentTarget.value)
+})
+
+document.querySelector('#habilidade').addEventListener('change', (ev) => {
+    console.log(ev.currentTarget.value)
+})
+
 const btnCriar = document.querySelector('#btn-criar')
 
 btnCriar.addEventListener('click', () => {
 
-    if(nome.value == '' || nivel.value == '' || especial.value == '' || tipo == '') {
+    /*if(nome.value == '' || nivel.value == '' || especial.value == '' || tipo == '') {
         alert('Todos os campos tem de ser preenchidos')
     } else if (nivel.value < 50 || nivel.value > 200) {
         alert('Valores do nível tem de ser maior que 50 e menor que 200')
@@ -37,5 +49,23 @@ btnCriar.addEventListener('click', () => {
     nome.value = ''
     nivel.value = ''
     especial.value = ''
-    document.querySelector('#tipo').value = ''
+    document.querySelector('#tipo').value = ''*/
+
+    console.log(nome.value)
+    console.log(nivel.value)
+    console.log(elemento)
+    console.log(habilidade)
 })
+
+/*Habilidades de Combate:
+Mestre em Espada: Habilidade em combate com espadas e outras armas brancas.
+Arco e Flecha: Competência em usar arcos e flechas para ataques à distância.
+Furtividade: Capacidade de se mover despercebido, útil para emboscadas e infiltração.
+Ladino: Habilidade em arrombar fechaduras, destreza manual e roubo.
+Tática: Conhecimento de estratégias e formações para combate. 
+Habilidades Mágicas:
+Mestre em Magia: Capacidade de manipular energias mágicas para diversos fins.
+Feitiçaria: Utilização de artes arcanas, incluindo invocações e manipulação de forças sobrenaturais.
+Alquimia: Criação de poções e itens mágicos.
+Encantamento: Infusão de propriedades mágicas em objetos.
+Divinação: Habilidade de prever o futuro ou obter conhecimento oculto.*/ 
